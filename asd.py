232f78fa24314658ec8177def1e3cbd113aadf9f
fikiham <82459624+fikiham@users.noreply.github.com>
2021-04-13 22:02:12 +0700

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,17 @@
+#method
+from dsa import Ac
+
+#variabel kondisi
+kondisi = input("On/Off")
+
+#perulangan while
+while True:
+    
+    #pengkondisian
+    if kondisi == "":
+        suhu = input("Masukkan suhu yang anda inginkan")
+        Ac.ubah(suhu)
+    elif kondisi == "Off":
+        break
+    else:
+        print("Anda salah memasukkan kondisi, bisa baca ga")
---
+++ dsa.py
@@ -0,0 +1,7 @@
+#class, function
+class Ac:
+    def __init__(self, suhu):
+        self.suhu = suhu
+    def ubah(suhu):
+        print(f"ubah suhu jadi {suhu} gatau")
+    
