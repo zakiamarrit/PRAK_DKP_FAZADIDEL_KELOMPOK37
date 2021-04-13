1bd3b4027cffc3c75b77f8834e8c04f9bb0f477e
fikiham <82459624+fikiham@users.noreply.github.com>
2021-04-13 22:23:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #method
 from functions import Ac
 
+run = True
 #perulangan while
-while True:
+while run:
     #variabel kondisi
     kondisi = input("On/Off")
     
     #pengkondisian
-    if kondisi == "On":  
+    if kondisi == "On":
         suhu = input("Masukkan suhu yang anda inginkan")
         Ac.ubah(suhu)
+        On = True
+        while On:
+            kondisi = input("Ganti suhu / Off")
+            if kondisi == "Ganti suhu":
+                suhu = input("Masukkan suhu yang anda inginkan")
+                Ac.ubah(suhu)
+            elif kondisi == "Off":
+                run = False
+            else:
+                print("Anda salah memasukkan kondisi, mohon baca kembali perintah yang tertera")
     elif kondisi == "Off":
         break
     else:
