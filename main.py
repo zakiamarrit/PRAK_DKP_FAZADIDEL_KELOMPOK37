58100b060a7ad8263bfbb1b911174a07d412d5e5
felisianarz <felisianaaraz31@gmail.com>
2021-04-15 10:43:00 +0700

    tambah method iden dan nama, tambah syntax

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 #method
+import functions
 from functions import Ac
 
-print("Kelompok = 37\n Shift = 2\n Anggota 1 = Abdullah Faqih\n Anggota 2 = Didi Suhardi\n Anggota 3 = Felisiana Ardelia\n Anggota 4 = Zakia Marrit\n")
-print("===== Pengatur Suhu AC=====")
+la = functions.iden("37","2")
+la.identitas()
+
+li = functions.nama("Abdullah Faqih Mubarak","Didi Suhardi","Felisiana Ardelia Azzahra","Zakia Marrit")
+li.anggota()
+
+print("================ Pengaturan AC ================")
 
 run = True
 #perulangan while
 while run:
     #variabel kondisi
-    kondisi = input("On/Off ? ")
+    kondisi = input("Apakah kamu ingin menyalakan AC (Ya/Tidak)? ")
     
     #pengkondisian
-    if kondisi == "On":
-        suhu = input("Masukkan suhu yang anda inginkan = ")
+    if kondisi == "Ya":
+        print ("AC menyala")
+        suhu = input("\nMasukkan suhu yang anda inginkan = ")
         Ac.ubah(suhu)
+
         On = True
-        while On:
-            kondisi = input("Ganti suhu / Tidak perlu ? ")
-            if kondisi == "Ganti suhu":
+        while On:            
+            kondisi = input("\nApakah kamu ingin mengganti suhu (Ya/Tidak)? ")
+            if kondisi == "Ya":
                 suhu = input("Masukkan suhu yang anda inginkan = ")
                 Ac.ubah(suhu)
-            elif kondisi == "Tidak perlu":
+
+            elif kondisi == "Tidak":
                 break
             else:
                 print("Anda salah memasukkan kondisi, mohon baca kembali perintah yang tertera")
-    elif kondisi == "Off":
+
+    elif kondisi == "Tidak":
+        print ("AC mati")
         break
     else:
-        print("Anda salah memasukkan kondisi, mohon baca kembali perintah yang tertera ya..")
+        print("Anda salah memasukkan kondisi, mohon baca kembali perintah yang tertera")
